005162540bcd663842980d29d0fec740553ea4fc
Cloud User <Subhadip Nag-2022wa86235>
2026-09-17 14:06:30 +0530

    Initial Maven project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testApplication() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
